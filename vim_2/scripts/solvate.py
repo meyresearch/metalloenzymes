d8091eef0534e8ed46a0b68d07cc9711246fec39
jasmin-guven <jasmin.guven@hotmail.com>
2022-11-04 17:54:01 +0000

    fixed ligand mol2 file, need to ad OH- and parameterise

--- vim_2/scripts/solvate.py
+++ vim_2/scripts/solvate.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings("ignore")
 import BioSimSpace as bss 
 import pandas as pd
+import MDAnalysis as mda
 
 
 def get_parmed_charge(parmed_file: str) -> float:
@@ -16,54 +17,20 @@ def get_parmed_charge(parmed_file: str) -> float:
     df = pd.DataFrame(info[1:], columns=column_headers)
     return df["Charge"].astype(float).sum()
 
-
-def get_mol2_info(mol2_file: str) -> tuple:
-    """
-    Get 3 lists containing the molecule, atom and remaining info from a mol2 file
-    """
-    remaining_info = None
-    with open(mol2_file, "r") as ifile:
-        mol2_lines = ifile.readlines()
-    get_index = lambda kword, lines: [i for i in range(len(lines)) if kword in lines[i]][0]
-    atom_info_index = get_index("ATOM", mol2_lines) + 1
-    molecule_info = mol2_lines[:atom_info_index]
-    try:
-        bond_info_index = get_index("BOND", mol2_lines)
-        remaining_info = mol2_lines[bond_info_index:]
-        atom_info = [element.split() for element in mol2_lines[atom_info_index:bond_info_index]]
-        # atom_info = mol2_lines[atom_info_index:bond_info_index]
-    except IndexError:
-        print(f"No bond info found for file {mol2_file}")
-        substructure_index = get_index("SUBSTRUCTURE", mol2_lines)
-        atom_info = [element.split() for element in mol2_lines[atom_info_index:substructure_index]]
-        # atom_info = mol2_lines[atom_info_index:substructure_index]
-        remaining_info = mol2_lines[substructure_index:]
-    return molecule_info, atom_info, remaining_info
-
-
 ligand_directory = "../inputs/ligands/"
 protein_directory = "../inputs/protein/"
 xtal_filename = ligand_directory + "ligand_8_xtal"
 parm_filename = protein_directory + "PT1.mol2"
 
-ligand_1 = bss.IO.readMolecules(xtal_filename+".sdf")[0]
-bss.IO.saveMolecules(xtal_filename,ligand_1, "mol2")
-xtal_molecule, xtal_atom, xtal_remaining = get_mol2_info(xtal_filename+".mol2")
-parm_molecule, parm_atom, parm_remaining = get_mol2_info(parm_filename)
+
+parm_u = mda.Universe(protein_directory+"PT1.mol2")
+xtal_u = mda.Universe(ligand_directory+"ligand_8_xtal.mol2")
+new_coords = xtal_u.atoms.positions
+new_universe = parm_u.load_new(ligand_directory+"ligand_8_xtal.mol2", format="MOL2")
+
+new_universe.select_atoms("all").write(ligand_directory+"fixed_ligand_8.mol2")
 
 
+ligand_1 = bss.IO.readMolecules(ligand_directory+"fixed_ligand_8.mol2")[0]
 
-
-
-
-x, y, z = 2, 3, 4
-
-# for i in range(len(parm_atom)):
-#     parm_atom[i][x] = xtal_atom[i][x]
-#     parm_atom[i][y] = xtal_atom[i][y]
-#     parm_atom[i][z] = xtal_atom[i][z]
-    
-# with open(ligand_directory+"test_8.mol2", "w") as ofile:
-#     ofile.writelines(parm_molecule)
-#     ofile.writelines(parm_atom)
-#     ofile.writelines(parm_remaining)
+# add hydroxide ions to the ligand to make the charge integer and then parameterise using gaff2!!!
