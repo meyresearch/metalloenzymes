202b26d84044015d28b6af214e8ffa540782e36a
jasmin-guven <jasmin.guven@hotmail.com>
2023-02-24 16:51:05 +0000

    prelim afe results

--- scripts/ligands_from_azuma.sh
+++ scripts/ligands_from_azuma.sh
@@ -2,6 +2,6 @@
 ligands=($(seq 1 1 16))
 for i in ${ligands[@]};
 do
-        rsync -av azuma:projects/first_year_project/kpc2/ligands/ligand_$i/lig_h_$i.mol2 $HOME/projects/metalloenzymes/all_ligands/vim2/ligand_$i.mol2
+        rsync -av azuma:projects/first_year_project/kpc2/ligands/ligand_$i/lig_h_$i.mol2 $HOME/projects/metalloenzymes/starmap/
 done
 
