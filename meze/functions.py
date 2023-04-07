8c83139c24feae3b48dc1f1c5d6540883fc2f4c2
jasmin-guven <jasmin.guven@hotmail.com>
2023-04-07 17:44:22 +0100

    split code for creating perturbation network

--- meze/functions.py
+++ meze/functions.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import glob
+import BioSimSpace as bss
 
 
 def file_exists(file):
@@ -22,23 +24,80 @@ def file_exists(file):
     return file
 
 
-def parse_cli_arguments():
+# def path_exists(path):
+#     """ 
+#     Check that given path exists
+    
+#     Parameters:
+#     -----------
+#     path: str
+#         full path 
+#     Return:
+#     -------
+#     path: str
+#         existing path
+#     """
+#     if path is None:
+#         path = os.getcwd()
+#     elif not os.path.isdir(path):
+#         raise argparse.ArgumentTypeError(f"{path} does not exist")
+#     return path
+
+
+def read_files(path):
     """
-    Function to handle all argparse CLI arguments
-    Return: 
+    Read and sort files in a given path
+
+    Parameters:
+    -----------
+    path: str
+        full path to directory containing files
+    Return:
     -------
-    parser.parse_args(): Namespace
-        parsed CLI arguments
+    list
+        sorted list of files in path  
     """
-    parser = argparse.ArgumentParser(description="MEtalloenZymE FF-builder for alchemistry")
+    return sorted(glob.glob(path))
 
 
-    parser.add_argument("-i",
-                        "--input-pdb",
-                        dest="protein",
-                        required=True, 
-                        type=file_exists,
-                        help="input pdb file for the metalloenzyme/protein")
+def get_names(path):
+    """
+    
+    """
+    file_name = path.split("/")[-1]
+    extension = file_name.split(".")[-1]
+    print(extension)
+    return file_name.replace("." + extension, "")
 
-    return parser.parse_args()
+
+def prepare_system(ligand_path, protein):
+    """
+    
+    """
+    print(ligand_path, protein)
+    # Adapted from dmbol.py:
+    ligand_files = read_files(f"{ligand_path}/*.sdf")
+    ligand_files += read_files(f"{ligand_path}/*.mol2")
+    if len(ligand_files) < 2:
+        raise IOError(f"Path {ligand_path} must contain at least two sdf or mol2 files.")
+    ligands = [bss.IO.readMolecules(file)[0] for file in ligand_files]
+
+    ligand_names = [get_names(filepath) for filepath in ligand_files]
+    print(ligand_names)
+
+
+    # read ligand files 
+    # ligand_files = sorted(glob.glob(f"{ligand_path}*.sdf"))
+    # ligands = [bss.IO.readMolecules(filepath)[0] for filepath in ligand_files]
+    # ligand_names = [filepath.split("/")[-1].replace(".sdf","") for filepath in ligand_files]
+    # transformations, lomap_scores = bss.Align.generateNetwork(ligands, plot_network=True, names=ligand_names, work_dir=ligand_path)
+
+    # perturbation_network_dict = {}
+    # transformations_named = [(ligand_names[transf[0]], ligand_names[transf[1]]) for transf in transformations]
+    # for transformation, score in zip(transformations_named, lomap_scores):
+    #     perturbation_network_dict[transformation] = score
+
+    # with open(ligand_path+f"/lomap_{system}.csv", "w") as lomap_out:
+    #     for key, value in perturbation_network_dict.items():
+    #         lomap_out.write(f"{key}: {value}\n")
 
--- meze/prepare_meze.py
+++ meze/prepare_meze.py
@@ -1,24 +1,46 @@
 import functions
 import argparse
+import ast
+import os
+
+def clean_arguments(arguments):
+    """
+    Check arguments and clean them
+
+    Parameters:
+    -----------
+    arguments: Namespace
+        command-line arguments
+    Return:
+    -------
+    cleaned Namespace of arguments
+    
+    """
+    if arguments.ligand_path is None:
+        arguments.ligand_path = os.getcwd()
+    elif not os.path.isdir(arguments.ligand_path):
+        raise argparse.ArgumentTypeError(f"{arguments.ligand_path} does not exist")
+    return arguments
 
 
 def main():
     parser = argparse.ArgumentParser(description="MEZE: MEtalloenZymE FF-builder for alchemistry")
+   
     parser.add_argument("-i",
                         "--input-pdb",
                         dest="protein",
                         required=True, 
                         type=functions.file_exists,
                         help="input pdb file for the metalloenzyme/protein")
+    
     parser.add_argument("-l",
                         "--ligand-path",
                         dest="ligand_path",
-                        required=True, 
-                        type=functions.file_exists,
                         help="path to ligand files")
     
     arguments = parser.parse_args()
-
+    arguments = clean_arguments(arguments)
+    functions.prepare_system(arguments.ligand_path, arguments.protein)
 
 if __name__ == "__main__":
     main()
