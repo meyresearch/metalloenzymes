13e84b9f212a2a5eda941b9b95e7601b3420d5d8
jasmin-guven <jasmin.guven@hotmail.com>
2023-04-07 16:29:04 +0100

    add ligand argument

--- meze/prepare_meze.py
+++ meze/prepare_meze.py
@@ -10,7 +10,13 @@ def main():
                         required=True, 
                         type=functions.file_exists,
                         help="input pdb file for the metalloenzyme/protein")
-
+    parser.add_argument("-l",
+                        "--ligand-path",
+                        dest="ligand_path",
+                        required=True, 
+                        type=functions.file_exists,
+                        help="path to ligand files")
+    
     arguments = parser.parse_args()
 
 
